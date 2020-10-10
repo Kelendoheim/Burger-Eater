81e4f78668d635c2fd49afff8b2f902256d1ddb1
TDWalsh <thomas.walsh736@gmail.com>
2020-10-10 15:38:08 -0400

    added database connection views and model

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Classic", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("BBQ Bacon", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mushroom Swiss", true);

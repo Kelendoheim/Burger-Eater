32b90c57cabfa5e3c6606f94c43d6df2a73ae55d
TDWalsh <thomas.walsh736@gmail.com>
2020-10-10 15:48:29 -0400

    added rest of functionality. now only need styling

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,7 +7,7 @@ var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  cat.all(function(data) {
+  burger.all(function(data) {
     var hbsObject = {
       burgers: data
     };
@@ -32,7 +32,7 @@ router.put("/api/burgers/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  cat.update({
+  burger.update({
     devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
